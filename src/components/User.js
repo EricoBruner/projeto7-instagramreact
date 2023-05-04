950ef4aee6e6df2f400e0e446445a9220e0ab944
EricoBruner <ericodasilvabruner@gmail.com>
2023-05-04 19:29:15 -0300

    finalizado função de alterar nome e imagem do usuário

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 export default function User() {
+    let [userName, setUserName] = useState("catanacomics");
+    let [userImg, setUserImg] = useState("assets/img/catanacomics.svg");
+
+    const editProfileName = () => {
+        let newUserName = prompt("Novo nome de usuário:")
+
+        if(newUserName) {
+            setUserName(newUserName);
+        }
+    }
+
+    const editProfileImg = () => {
+        let newUserImg = prompt("Nova imagem de usuário:")
+
+        if(newUserImg) {
+            setUserImg(newUserImg);
+        }
+    }
+
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+            <img onClick={editProfileImg} src={userImg} alt="imagem de perfil"/>
             <div class="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{userName}</strong>
+                    <ion-icon onClick={editProfileName} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
