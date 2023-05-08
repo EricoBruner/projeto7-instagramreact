556b0264e93a88c230ecd53357438b20f3bbd780
EricoBruner <ericodasilvabruner@gmail.com>
2023-05-08 16:27:55 -0300

    retorando ao código anterior

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import reactDom from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-reactDom.render(<App />, document.querySelector(".root"))
+const root = createRoot(document.querySelector(".root"))
+
+root.render(<App />)
